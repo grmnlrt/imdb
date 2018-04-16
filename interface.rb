1d08328877aa93b837d0684a9454513cdec86f45
Germain Loret <germain.loret@gmail.com>
2018-04-16 15:11:18 +0200

    restart

--- interface.rb
+++ interface.rb
@@ -1,17 +1,1 @@
-require 'yaml'
 require_relative 'scraper'
-
-puts "Fetching URLs"
-urls = fetch_movies_urls
-
-movies = urls.map do |url|
-  puts "Scraping #{url}"
-  scrape_movie(url)
-end
-
-puts "Writing movies.yml"
-File.open("movies.yml", "w") do |f|
-  f.write(movies.to_yaml)
-end
-
-puts "Done."
